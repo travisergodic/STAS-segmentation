7f21d8ef99cbfd0324ac7eac69447714261944e3
Ubuntu <ubuntu@ip-172-31-5-76.ap-northeast-1.compute.internal>
2022-05-20 00:15:11 +0000

    add main.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import torch
 from torch import optim
+import segmentation_models_pytorch as smp
+from vision_transformer import SwinUnet
+from transformers import MaskFormerModel
+
+# device 
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # config
 train_image_dir = './data/Train_Images/'
@@ -8,6 +14,7 @@ seed = 14
 
 # preprocess
 ann_suffix = '.npz'
+img_suffix = '.jpg'
 train_img_size = (384, 384)
 test_img_size = (384, 384)
 h_flip_p=0.5
@@ -18,6 +25,7 @@ affine_p=0.
 train_ratio = 0.85
 train_batch_size = 16
 test_batch_size = 32
+num_workers = 4
 
 # train config 
 num_epoch = 100
@@ -29,6 +37,38 @@ optim_dict = {
     # 'weight_decay': 1e-2
 }
 
-# device 
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+## model 
+model = smp.Unet(
+    encoder_name="tu-tf_efficientnetv2_l_in21k",     
+    encoder_weights="imagenet",     
+    in_channels=3,                  
+    classes=1,
+    decoder_attention_type='scse'
+    ).to(DEVICE)
 
+# 384, 480
+# tu-tf_efficientnetv2_l_in21k
+# model = MaskFormerModel.from_pretrained("facebook/maskformer-swin-base-ade")
+# model = SwinUnet().to(DEVICE)
+
+## save
+save_config = {
+    "path": './resunet.pt', 
+    "freq": 1
+}
+
+## loss function 
+loss_fn = smp.utils.losses.DiceLoss(activation='sigmoid')
+
+# lm.GDiceLossV2(nn.Softmax(dim=1), do_bg=False, smooth=1e-5)
+# lm.IoULoss(nn.Softmax(dim=1), batch_dice=True, do_bg=False, smooth=1e-6, square=True)
+# lm.SoftDiceLoss(nn.Softmax(dim=1), batch_dice=True, do_bg=False, smooth=1e-6, square=True)
+# lm.BceLoss()
+# lm.BceDiceLoss()
+
+## metric
+metric_dict = {
+    'IOU': smp.utils.metrics.IoU(threshold=0.5),
+    'Accuracy': smp.utils.metrics.Accuracy(), 
+    'DICE': smp.utils.losses.DiceLoss(activation='sigmoid')
+}
---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import time
+import numpy as np 
+import glob
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from trainer import Trainer
+from data import StasDataset, Train_Preprocessor, Test_Preprocessor
+from config import *
+
+
+if __name__ == "__main__":
+    np.random.seed(seed)
+    image_path_list = sorted(glob.glob(train_image_dir + "*" + img_suffix))
+    split_index = int(len(image_path_list) * train_ratio)
+    train_path_list = image_path_list[:split_index]
+    test_path_list = image_path_list[split_index:]
+
+    # dataset & dataloader
+    train_data = StasDataset(train_path_list, label_dir, image_transform=Train_Preprocessor(train_img_size, 
+                                                                                            h_flip_p=h_flip_p, 
+                                                                                            v_flip_p=v_flip_p))
+    test_data = StasDataset(test_path_list, label_dir, image_transform=Test_Preprocessor(test_img_size))
+
+    train_dataloader = DataLoader(train_data, batch_size=train_batch_size, shuffle=True, num_workers=num_workers)
+    test_dataloader = DataLoader(test_data, batch_size=test_batch_size, num_workers=num_workers)
+
+    # train_model
+    start = time.time()
+    train_pipeline = Trainer()
+    train_pipeline.compile(optim_cls, decay_fn, loss_fn, metric_dict, is_sam, DEVICE, **optim_dict)
+    train_pipeline.fit(model, train_dataloader, test_dataloader, num_epoch, save_config)
+    print(f"Training takes {time.time() - start} seconds!")
