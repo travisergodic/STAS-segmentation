482269bb5716764930388d470f0c9aeeb56867b4
Ubuntu <ubuntu@ip-172-31-5-76.ap-northeast-1.compute.internal>
2022-05-22 07:52:21 +0000

    delete any voting and fix multiscale mode

--- evaluate.py
+++ evaluate.py
@@ -18,15 +18,25 @@ class Evaluator:
         self.device = device
     
     @torch.no_grad()
-    def _predict(self, path, mask_mode='color', do_tta=True, vote_mode='any', multiscale_list=None):         
+    def _predict(self, path, mask_mode='color', do_tta=True, multiscale_list=None):         
         x = self.image_transform(Image.open(path).convert('RGB'), None)[0].to(self.device)
-        if do_tta: 
-            X, tta_fns = self._forward_TTA(x, multiscale_list)
-            mask = self._backward_TTA(self.model(X), tta_fns)
-            if vote_mode == 'any': 
-                mask = (mask > 0.5).any(dim=0).squeeze()
-            elif vote_mode == 'soft': 
-                mask = (mask.mean(dim=0).squeeze() > 0.5)
+        
+        # do test time augmentation
+        if do_tta:
+            # only do four flips 
+            if multiscale_list is None: 
+                X, tta_fns = self._forward_TTA(x, None)
+                mask = self._backward_TTA(self.model(X), tta_fns).mean(dim=0).squeeze() > 0.5
+            # do four flips + multiscale testing 
+            else: 
+                origin_size = x.size()[1:] 
+                X_dict, tta_fns = self._forward_TTA(x, multiscale_list)
+                mask = torch.zeros(*origin_size)
+                for X in X_dict: 
+                    mask += Resize(origin_size, interpolation=InterpolationMode.NEAREST)(self._backward_TTA(self.model(X), tta_fns).mean(dim=0)).squeeze()
+                mask = mask/len(X_dict) > 0.5 
+                
+        # no test time augmentation
         else: 
             mask = self.model(x.unsqueeze(0)).squeeze() > 0.5
 
@@ -38,8 +48,8 @@ class Evaluator:
             self._check_range(mask, [0, 1])
         return mask
     
-    def evaluate(self, image_paths, label_paths, do_tta=True, vote_mode='soft', multiscale_list=None): 
-        if isinstance(image_paths, str) and isinstance(label_paths, str): 
+    def evaluate(self, image_paths, label_paths, do_tta=True, multiscale_list=None): 
+        if type(image_paths) == str and type(label_paths) == str: 
             assert os.path.isdir(image_paths) and os.path.isdir(label_paths), f"{image_paths} or {label_paths} is not a directory!"
             image_paths = sorted([os.path.join(image_paths, basename) for basename in os.listdir(image_paths)])
             label_paths = sorted([os.path.join(label_paths, basename) for basename in os.listdir(label_paths)])
@@ -51,16 +61,15 @@ class Evaluator:
         self.model.eval()
         for image_path, label_path in tqdm(list(zip(image_paths, label_paths))): 
             gt = torch.from_numpy(np.load(label_path)['image']).to(self.device)
-            mask = self._predict(image_path, mask_mode='class', do_tta=do_tta, vote_mode=vote_mode, multiscale_list=multiscale_list)
+            mask = self._predict(image_path, mask_mode='class', do_tta, multiscale_list)
             mask = Resize(gt.shape, InterpolationMode.NEAREST)(mask.unsqueeze(0)).squeeze()
             total_score += Evaluator.dice_score(mask, gt)
         return total_score/len(image_paths)            
     
-    def make_prediction(self, paths, save_dir, mask_mode='color', do_tta=True, vote_mode='soft', multiscale_list=None): 
+    def make_prediction(self, paths, save_dir, mask_mode='color', do_tta=True, multiscale_list=None): 
         assert mask_mode in ('color', 'class')
-        assert vote_mode in ('any', 'soft')
         assert hasattr(paths, '__iter__'), "Input Variable paths is not iterable!"
-        if isinstance(paths, str): 
+        if type(paths) == str: 
             assert os.path.isdir(paths), "Input variable 'path' is not a directory!"
             img_dir = paths
             paths = [os.path.join(paths, basename) for bansename in os.listdir(paths)]
@@ -74,7 +83,7 @@ class Evaluator:
         
         self.model.eval()
         for path in tqdm(paths): 
-            mask = self._predict(path, mask_mode, do_tta, vote_mode, multiscale_list=multiscale_list)
+            mask = self._predict(path, mask_mode, do_tta, multiscale_list)
             mask = Resize(origin_size, InterpolationMode.NEAREST)(mask.unsqueeze(0)).squeeze()
             self._save(mask.cpu().numpy().astype(np.uint8), os.path.basename(path).split(".")[0] + ".png", save_dir)
         
@@ -93,12 +102,13 @@ class Evaluator:
                 assert isinstance(multiscale, int) and multiscale % 32 == 0
         
     def _forward_TTA(self, x, multiscale_list=None):
-        tta_fns = [[x, y] for x in [lambda x: x, hflip] for y in [lambda x: x, vflip]]
-        if multiscale_list is not None:  
-            tta_fns = [[x, y, z] for x, y in tta_fns for z in [Resize(size, interpolation=InterpolationMode.NEAREST) for size in multiscale_list]]
-        tta_fns = [Compose(ele) for ele in tta_fns]
-        res = [tta_fn(x) for tta_fn in tta_fns]
-        return torch.stack(res, dim=0), tta_fns 
+        tta_fns = [Compose([x, y]) for x in [lambda x: x, hflip] for y in [lambda x: x, vflip]]
+        if multiscale_list is None:
+            return torch.stack([tta_fn(x) for tta_fn in tta_fns], dim=0), tta_fns
+        res_dict = dict()
+        for scale in multisclae_list:
+            res_dict[scale] = torch.stack([Resize((scale, scale), interpolation=InterpolationMode.NEAREST)(tta_fn(x)) for tta_fn in tta_fns], dim=0)
+        return res_dict, tta_fns
     
     def _backward_TTA(self, X, tta_fns):
         return torch.stack(
